9b9e3a7486b943f1d6fd0242a2ae91095e787cb0
mapkeKirito <myzhouye@gmail.com>
2017-04-20 22:52:06 +0800

    Initial commit

---
+++ app/src/main/java/com/demo01/maple/androidgithubtest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.demo01.maple.androidgithubtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
